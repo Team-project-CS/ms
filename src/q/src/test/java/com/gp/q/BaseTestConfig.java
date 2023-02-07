98fcd3d1f5afa692c0fd42662d6039f0bd6bdc54
Ilya <iliaaa377@gmail.com>
2023-02-07 13:38:34 +0300

    fixed test problem with bean injection

---
+++ src/q/src/test/java/com/gp/q/BaseTestConfig.java
@@ -0,0 +1,9 @@
+package com.gp.q;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.gp.q")
+public class BaseTestConfig {
+}
