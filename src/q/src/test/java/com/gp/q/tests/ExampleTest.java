634f8a4ab8ffa71312eff92561fa7dacc1e339dc
Ilya <iliaaa377@gmail.com>
2023-02-07 11:56:07 +0300

    fixed test

---
+++ src/q/src/test/java/com/gp/q/tests/ExampleTest.java
@@ -0,0 +1,14 @@
+package com.gp.q.tests;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+public class ExampleTest {
+
+    @Test
+    void testExample() {
+        Assertions.assertEquals(1, 1);
+    }
+}
