61a87537a418a8450b675615fdda0e00163e822f
mizarion <zsgluschenko@edu.hse.ru>
2023-02-10 15:43:55 +0300

    Добавил описание API для swagger. Удалил PutMapping.

--- src/q/src/main/java/com/gp/q/model/dto/QueueMessageDto.java
+++ src/q/src/main/java/com/gp/q/model/dto/QueueMessageDto.java
@@ -1,5 +1,8 @@
 package com.gp.q.model.dto;
 
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,13 +23,23 @@ public class QueueMessageDto {
      * <br>
      * <a href="https://www.rabbitmq.com/queues.html#names">Queue names may be up to 255 bytes of UTF-8 characters </a>
      */
-//    @NotBlank
+    @ApiModelProperty(
+            example = "queue1",
+            value = "Queue name. Must not start with 'amq.' and be greater than 255 characters. " +
+                    "Available aliases: {'name', 'queue', 'queue_name'}")
     @Pattern(regexp = "^(?!amq[.]).*$")
     @Size(min = 1, max = 255)
+    @JsonProperty("name")
+    @JsonAlias({"queue_name", "queue"})
     private String name;
     /**
      * Полезная нагрузка
      */
+    @ApiModelProperty(
+            example = "*****",
+            value = "Message written to or read from the queue. Available aliases: {'message', 'payload'}")
     @NotNull
+    @JsonProperty("message")
+    @JsonAlias("payload")
     private String message;
 }
