42a7af7116aef8404c9471fdf609902497e841fa
mizarion <zsgluschenko@edu.hse.ru>
2023-02-15 18:25:37 +0300

    #16 Добавил регистрацию в eureka для api и q

--- src/api/src/main/java/com/gp/api/ApiBootApplication.java
+++ src/api/src/main/java/com/gp/api/ApiBootApplication.java
@@ -3,8 +3,10 @@ package com.gp.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class ApiBootApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
--- src/q/src/main/java/com/gp/q/MQBootApplication.java
+++ src/q/src/main/java/com/gp/q/MQBootApplication.java
@@ -3,8 +3,10 @@ package com.gp.q;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 class MQBootApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
