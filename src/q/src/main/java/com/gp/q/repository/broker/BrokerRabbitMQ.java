68ea7e3b8488362be6374f7f2478cb503144ce02
mizarion <zsgluschenko@edu.hse.ru>
2023-03-14 18:01:25 +0300

    #28 Добавил проверку на существование очереди перед вставкой в нее

--- src/q/src/main/java/com/gp/q/repository/broker/BrokerRabbitMQ.java
+++ src/q/src/main/java/com/gp/q/repository/broker/BrokerRabbitMQ.java
@@ -4,21 +4,33 @@ import com.gp.q.exception.throwables.QueueServiceException;
 import com.gp.q.model.entity.QueueMessageEntity;
 import com.gp.q.repository.MQBroker;
 import lombok.SneakyThrows;
+import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Repository;
+
+import javax.validation.constraints.NotNull;
 
 @Repository
 public class BrokerRabbitMQ implements MQBroker {
 
     private final RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    private AmqpAdmin admin;
+
     public BrokerRabbitMQ(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
 
     @Override
-    public QueueMessageEntity push(QueueMessageEntity entity) {
+    @SneakyThrows
+    public QueueMessageEntity push(@NotNull QueueMessageEntity entity) {
+        // Проверяем наличие очереди
+        if (admin.getQueueInfo(entity.getName()) == null) {
+            throw new QueueServiceException("queue with name '" + entity.getName() + "' not exist", HttpStatus.NOT_FOUND);
+        }
         // Если применяется обменник по умолчанию,
         // то сообщение будет маршрутизироваться в очередь с именем равным ключу маршрутизации сообщения.
         rabbitTemplate.convertAndSend(entity.getName(), entity.getMessage());
