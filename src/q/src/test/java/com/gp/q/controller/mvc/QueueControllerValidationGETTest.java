3364096e31b99475faf609273aa3e5cf79f3f017
mizarion <zsgluschenko@edu.hse.ru>
2023-03-14 15:34:13 +0300

    #24 Вынес mockMvc тесты на контроллер

--- src/q/src/test/java/com/gp/q/controller/mvc/QueueControllerValidationGETTest.java
+++ src/q/src/test/java/com/gp/q/controller/mvc/QueueControllerValidationGETTest.java
@@ -1,6 +1,7 @@
-package com.gp.q.controller;
+package com.gp.q.controller.mvc;
 
 import com.gp.q.service.QueueService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -8,32 +9,23 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureMockMvc
 @SpringBootTest
-class QueueHistoryControllerValidationTest {
-
-    public static final String PATH = "/queue/history";
+class QueueControllerValidationGETTest {
     @MockBean
     private QueueService service;
 
     @Autowired
     private MockMvc mockMvc;
 
-    private static final String getAllMessagesByPeriodAndName = """
-            {
-              "name": "queue1",
-              "start": "2023-02-13T14:55:18.497007",
-              "end": "2023-02-13T14:55:38.497007"
-            }""";
-
-
     @Test
-    void getMessagesByQueueName() throws Exception {
-        mockMvc.perform(get(PATH + "/queue1")
+    void getQueue() throws Exception {
+        mockMvc.perform(get("/queue" + "?queue=" + "queue_1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
                 )
@@ -41,12 +33,12 @@ class QueueHistoryControllerValidationTest {
     }
 
     @Test
-    void getMessagesByDate() throws Exception {
-        mockMvc.perform(get(PATH)
+    void getQueueById() throws Exception {
+        mockMvc.perform(get("/queue" + "?id=" + "queue_1")
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON)
-                        .content(getAllMessagesByPeriodAndName)
                 )
-                .andExpect(status().is2xxSuccessful());
+                .andExpect(status().is4xxClientError())
+                .andExpect(result -> Assertions.assertTrue(result.getResolvedException() instanceof MissingServletRequestParameterException));
     }
 }
